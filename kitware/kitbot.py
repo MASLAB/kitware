726f4ef2c5dd60428905c2e3a1f41eed29b5b5e9
John <johnzhang15@gmail.com>
2022-01-05 02:23:25 -0500

    Code for running new DFRobot motor controllers. Untested. TODO: write new motor object for TAMProxy.

--- kitware/kitbot.py
+++ kitware/kitbot.py
@@ -3,15 +3,24 @@
 import rclpy
 from kitware.msg import DriveCmd
 from tamproxy import ROS2Sketch
-from tamproxy.devices import AnalogInput, Motor
+from tamproxy.devices import DigitalOutput, AnalogOutput
 
 
 class KitBotNode(ROS2Sketch):
     """ROS2 Node that controls the KitBot via the Teensy and tamproxy"""
+    # new dual channel DC motor controller
+    # https://wiki.dfrobot.com/Dual-Channel_DC_Motor_Driver-12A_SKU:DFR0601
+    # TODO: implement TAMProxy Motor object
 
     # Pin mappings
-    LMOTOR_PINS = (2, 3)  # DIR, PWM
-    RMOTOR_PINS = (4, 5)  # DIR, PWM
+    # LMOTOR_PINS = (2, 3, 4)  # INA1, INB1, PWM1
+    INA1_PIN = 2
+    INB1_PIN = 3
+    PWM1_PIN = 4
+    # RMOTOR_PINS = (5, 6, 7)  # INA2, INB2, PWM2
+    INA2_PIN = 5
+    INB2_PIN = 6
+    PWM2_PIN = 7
 
     def setup(self):
         """
@@ -26,9 +35,15 @@ class KitBotNode(ROS2Sketch):
             10)
         self.drive_sub  # prevent unused variable warning
 
-        # Create the motor objects
-        self.lmotor = Motor(self.tamp, *self.LMOTOR_PINS)
-        self.rmotor = Motor(self.tamp, *self.RMOTOR_PINS)
+        # create pin objects
+        # left motor
+        self.INA1 = DigitalOutput(self.tamp, self.INA1_PIN)
+        self.INB1 = DigitalOutput(self.tamp, self.INB1_PIN)
+        self.PWM1 = AnalogOutput(self.tamp, self.PWM1_PIN)
+        # right motor
+        self.INA2 = DigitalOutput(self.tamp, self.INA2_PIN)
+        self.INB2 = DigitalOutput(self.tamp, self.INB2_PIN)
+        self.PWM2 = AnalogOutput(self.tamp, self.PWM2_PIN)
 
     def speed_to_dir_pwm(self, speed):
         """Converts floating point speed (-1.0 to 1.0) to dir and pwm values"""
@@ -37,8 +52,28 @@ class KitBotNode(ROS2Sketch):
 
     def drive_callback(self, msg):
         """Processes a new drive command and controls motors appropriately"""
-        self.lmotor.write(*self.speed_to_dir_pwm(msg.l_speed))
-        self.rmotor.write(*self.speed_to_dir_pwm(msg.r_speed))
+        # logic for direction
+        # INAx: L INBx: L   Brake
+        # INAx: L INBx: H   Rotate Forward
+        # INAx: L INBx: H   Rotate Reverse
+        # INAx: H INBx: H   Free Wheel
+
+        if msg.l_speed > 0: # forward
+            self.INA1.write(False)
+            self.INB1.write(True)
+        else # reverse
+            self.INA1.write(True)
+            self.INB1.write(False)
+
+        if msg.r_speed > 0:
+            self.INA2.write(False)
+            self.INB2.write(True)
+        else
+            self.INA2.write(True)
+            self.INB2.write(False)
+
+        self.PWM1.write(*self.speed_to_dir_pwm(msg.l_speed)) # left motor
+        self.PWM2.write(*self.speed_to_dir_pwm(msg.r_speed)) # right motor
 
 if __name__ == '__main__':
     rclpy.init()
