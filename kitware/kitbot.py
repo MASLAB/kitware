7e78db7718b70d9b3ce881bebd3eb7b58c9becf6
johnzzhang <johnz@mit.edu>
2023-01-19 17:46:06 -0500

    Working visionbot code.

--- kitware/kitbot.py
+++ kitware/kitbot.py
@@ -3,20 +3,14 @@
 import rclpy
 from kitware.msg import DriveCmd
 from tamproxy import ROS2Sketch
-from tamproxy.devices import DigitalOutput, AnalogOutput, Servo, DFRMotor
+from tamproxy.devices import Servo, Motor
 
 class KitBotNode(ROS2Sketch):
     """ROS2 Node that controls the KitBot via the Teensy and tamproxy"""
-    # new dual channel DC motor controller
-    # https://wiki.dfrobot.com/Dual-Channel_DC_Motor_Driver-12A_SKU:DFR0601
     # Pin mappings
-    INA1_PIN = 2
-    INB1_PIN = 4
-    PWM1_PIN = 6
-    # RMOTOR_PINS = (5, 6, 7)  # INA2, INB2, PWM2
-    INA2_PIN = 3
-    INB2_PIN = 5
-    PWM2_PIN = 7
+    LMOTOR_PINS = (2, 3)  # DIR, PWM
+    RMOTOR_PINS = (4, 5)  # DIR, PWM
+
     # SERVO_PIN
     SERVO_PIN = 9
 
@@ -33,17 +27,12 @@ class KitBotNode(ROS2Sketch):
             10)
         self.drive_sub  # prevent unused variable warning
 
-        # create pin objects
-        # left motor
-        self.INA1 = DigitalOutput(self.tamp, self.INA1_PIN)
-        self.INB1 = DigitalOutput(self.tamp, self.INB1_PIN)
-        self.PWM1 = AnalogOutput(self.tamp, self.PWM1_PIN)
-        # right motor
-        self.INA2 = DigitalOutput(self.tamp, self.INA2_PIN)
-        self.INB2 = DigitalOutput(self.tamp, self.INB2_PIN)
-        self.PWM2 = AnalogOutput(self.tamp, self.PWM2_PIN)
-        # servo
+        # create the motor objects
+        self.lmotor = Motor(self.tamp, *self.LMOTOR_PINS)
+        self.rmotor = Motor(self.tamp, *self.RMOTOR_PINS)
+        # create servo object
         self.servo = Servo(self.tamp, self.SERVO_PIN)
+        # zero servo position
         self.servo.write(0)
 
     def speed_to_dir_pwm(self, speed):
@@ -53,19 +42,8 @@ class KitBotNode(ROS2Sketch):
 
     def drive_callback(self, msg):
         """Processes a new drive command and controls motors appropriately"""
-        self.left_motor.write(*self.speed_to_dir_pwm(msg.l_speed))
-        self.right_motor.write(*self.speed_to_dir_pwm(msg.r_speed))
-
-        if msg.r_speed > 0:
-            self.INA2.write(False)
-            self.INB2.write(True)
-
-        else: # reverse
-            self.INA2.write(True)
-            self.INB2.write(False)
-
-        self.PWM1.write(self.speed_to_dir_pwm(msg.l_speed)) # left motor
-        self.PWM2.write(self.speed_to_dir_pwm(-msg.r_speed)) # right motor
+        self.lmotor.write(*self.speed_to_dir_pwm(msg.l_speed))
+        self.rmotor.write(*self.speed_to_dir_pwm(msg.r_speed))
         self.servo.write(msg.servo_pos) # servo
 
 if __name__ == '__main__':
